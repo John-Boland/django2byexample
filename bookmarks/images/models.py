c53d6b6cb5556866ad683fdef26552805205a3ff
johnboland91 <johnboland91@gmail.com>
2019-06-29 14:59:03 -0500

    Adding redis for storing item views

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
+
 
 class Image(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
@@ -17,6 +19,8 @@ class Image(models.Model):
     users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
                                         related_name='images_liked',
                                         blank=True)
+    total_likes = models.PositiveIntegerField(db_index=True,
+                                              default=0)
 
     def __str__(self):
         return self.title
@@ -25,3 +29,6 @@ class Image(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         super(Image, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+            return reverse('images:detail', args=[self.id, self.slug])
