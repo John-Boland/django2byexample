9b0c4dcb7642569f8c0fdcfa3bb412ce7b431db7
johnboland91 <johnboland91@gmail.com>
2019-06-29 19:22:53 -0500

    Create cart class for storing items in session

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -110,3 +111,5 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CART_SESSION_ID = 'cart'
