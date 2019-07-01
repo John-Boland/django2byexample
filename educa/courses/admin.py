1e0297561c7050f8338a728c7f604b5a1f4d9e86
johnboland91 <johnboland91@gmail.com>
2019-06-30 23:53:34 -0500

    Create models for diverse content

--- educa/courses/admin.py
+++ educa/courses/admin.py
@@ -6,7 +6,7 @@ class SubjectAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug']
     prepopulated_fields = {'slug': ('title',)}
 
-class ModuleInline(admin.StackedInline)
+class ModuleInline(admin.StackedInline):
     model = Module
 
 @admin.register(Course)
