f7c8789f415fa4161ecb02e3dd102fce4f80fa1a
johnboland91 <johnboland91@gmail.com>
2019-07-02 05:46:53 -0500

    rest

--- educa/courses/api/urls.py
+++ educa/courses/api/urls.py
@@ -14,11 +14,8 @@ urlpatterns = [
 
     path('subjects/<pk>',
         views.SubjectDetailView.as_view(),
-        name='subject_detail'),
-
-    path('courses/<pk>/enroll/',
-        views.CourseEnrollView.as_view(),
-        name='course_enroll'),
+        name='subject_detail'), 
 
     path('', include(router.urls)),
+
 ]
---
+++ educa/educa/settings/pro.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DEBUG = False
+
+ADMINS = (
+    ('John Boland', 'johnboland91@gmail.com'),
+)
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {    
+    }
+}
