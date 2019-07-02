03eea4145fb5ec6ce262f84ceff8d6a5a06faa5e
johnboland91 <johnboland91@gmail.com>
2019-07-02 04:16:56 -0500

    Build list and detail api views

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'embed_video',
     'memcache_status',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -141,3 +142,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CACHE_MIDDLEWARE_ALIAS = 'default'
 CACHE_MIDDLEWARE_SECONDS = 60 * 15 # 15 minutes
 CACHE_MIDDLEWARE_KEY_PREFIX = 'educa'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+ 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
